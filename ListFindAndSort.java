a9cfd1eeafd1feab4be1a13c83f4e1de4cb1b8de
NazeCoder <72634265+NazeCoder@users.noreply.github.com>
2021-04-20 12:55:29 +0800

    列表的排序和索引方法
    这里是关于int列表的排序和索引方法。

--- ListFindAndSort.java
+++ ListFindAndSort.java
@@ -3,7 +3,7 @@ import java.util.Arrays;
 public class _01ListSortAndFind {
 //以下实例演示了如何使用sort()方法对Java数组进行排序，及如何使用 binarySearch() 方法来查找数组中的元素, 这边我们定义了 printArray() 方法
 	public static void main(String[] args) {
-		int[] list = {1,24,56,3,5,-1,3,-5,55,-65,98,24,6,-9};
+		int[] list = {1,24,56,3,5,-1,3,-5,55,55,-65,98,24,6,-9};
 		printArray("原数组为",list);
 		Arrays.sort(list);
 		printArray("排序后的数组为",list);
